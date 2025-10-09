be32b6bf8a7e78cf3ce0727f46462cfd5a890bb0
Aaron Hill <aaron@tensorzero.com>
2025-10-09 13:58:15 +0200

    Bad lean error example

--- LeanUtils/KernelCheck.lean
+++ LeanUtils/KernelCheck.lean
@@ -45,32 +45,44 @@ inductive KernelCheckResult where
 | error (e: String)
 
 
-def findTargetEnv (trees: List (InfoTree)) (targetSorry: ParsedSorry) : IO (Environment × TheoremVal) := do
-  let matchingTrees := trees.filter (fun t => match t with
-    | .context ctx t => ((ctx.mergeIntoOuter? none).map (fun p => p.parentDecl? == some (targetSorry.parentDecl))).getD false
-    | _ => false
+structure TargetEnvData where
+  env: Environment
+  theoremVal: TheoremVal
+  type: Expr
+
+def findTargetEnv (trees: List (InfoTree)) (targetSorry: ParsedSorry) : IO TargetEnvData := do
+  let matchingTrees := trees.filterMap (fun t => match t with
+    | .context ctx t => if (((ctx.mergeIntoOuter? none).map (fun p => p.parentDecl? == some (targetSorry.parentDecl))).getD false) then (ctx.mergeIntoOuter? none).map (fun ctx => (t, ctx)) else none
+    | _ => none
   )
 
-  let tree := if matchingTrees.length == 1 then matchingTrees[0]! else (panic "Expected exactly one one matching tree, found {matchingTrees}")
-  let a ← tree.visitM (m := Id) (postNode := fun ctx i cs as => do match i with
+  let [(tree, ctxInfo)] := matchingTrees | (throw (IO.userError "Expected exactly one one matching tree, found {matchingTrees}"))
+  -- TODO - explain why an empty LocalContext is okay. Maybe - local context occurs within TermElabM - we're at top-level decl, so no local context
+  let a ← ctxInfo.runMetaM {} (do (tree.visitM (m := MetaM) (postNode := fun ctx i cs as => match i with
     -- TODO - deduplicate this
-    | .ofTermInfo ti => pure ((as.flatMap Option.toList).flatten ++ (if targetSorry.pos == ctx.fileMap.toPosition ti.stx.getPos?.get! then ([ctx]) else []))
-    | .ofTacticInfo ti =>  pure ((as.flatMap Option.toList).flatten ++ (if targetSorry.pos == ctx.fileMap.toPosition ti.stx.getPos?.get! then ([ctx]) else []))
+    | .ofTermInfo ti => do pure ((as.flatMap Option.toList).flatten ++ (if targetSorry.pos == ctx.fileMap.toPosition ti.stx.getPos?.get! then ([(ctx, ti.expectedType?)]) else []))
+    | .ofTacticInfo ti =>
+      let head := (as.flatMap Option.toList).flatten
+      if targetSorry.pos == ctx.fileMap.toPosition ti.stx.getPos?.get! then do
+        let type ← if let [goal] := ti.goalsBefore then goal.getType else (throwError ("Found more than one goal"))
+        return head ++ ([(ctx, some type)])
+      else
+        return head
     | _ => pure []
-  )
-
+  )))
 
   let matchedCtxs := a.get!
   match matchedCtxs with
-  | [ctx] =>
+  | [(ctx, some type)] =>
       if let some oldDecl :=  ctx.env.find? targetSorry.parentDecl then
         match oldDecl with
         | .thmInfo info =>
-          pure (ctx.env, info)
+          pure {env := ctx.env, theoremVal := info, type := type}
         | _ =>
           throw (IO.userError "Unexpected constant type")
       else
         throw (IO.userError "Misisng parentDecl in environment")
+  | [(ctx, none)] => throw (IO.userError "Missing expected type for sorry")
   | _ => throw (IO.userError "Expected exactly one ctx")
 
 /-
@@ -78,17 +90,16 @@ check that `expr` has type `type`
 -/
 -- TODO - change the error type to make it harder to accidentally return success
 -- remove the 'panics'
-def kernelCheck (sorryFilePath: System.FilePath) (expr type : SerializedExpr) (targetSorry: ParsedSorry) (bannedNames : List Name) : IO (KernelCheckResult) := do
+def kernelCheck (sorryFilePath: System.FilePath) (expr : SerializedExpr) (type: Expr) (targetSorry: ParsedSorry) (bannedNames : List Name) : IO (KernelCheckResult) := do
   let expr := deserializeExpr expr
-  let type := deserializeExpr type
   let (fileMap, trees) ← extractInfoTrees sorryFilePath
-  let (newEnv, val) ← findTargetEnv trees targetSorry
-  let _ ← Core.CoreM.toIO (ctx := {fileName := sorryFilePath.fileName.get!, fileMap := fileMap}) (s := { env := newEnv }) do
+  let targetData ← findTargetEnv trees targetSorry
+  let _ ← Core.CoreM.toIO (ctx := {fileName := sorryFilePath.fileName.get!, fileMap := fileMap}) (s := { env := targetData.env }) do
     if expr.containsConstantNames bannedNames then
       return (KernelCheckResult.error "contains banned constant name.")
     else
       try
-        addDecl (Declaration.thmDecl {val with value := expr, type := type, name := ← mkFreshId})
+        addDecl (Declaration.thmDecl {sdjfsdf.theoremVal with value := expr, type := type, name := ← mkFreshId})
         return (KernelCheckResult.success)
       catch e =>
         return (KernelCheckResult.error (← e.toMessageData.toString))
@@ -96,19 +107,21 @@ def kernelCheck (sorryFilePath: System.FilePath) (expr type : SerializedExpr) (t
   return KernelCheckResult.success
 
 def fakeMain (args : List String) : IO UInt32  := do
-  if let some path := args[0]? then
+  if let [path, rawExpr] := args then
     IO.println "Running sorry extraction."
     unsafe enableInitializersExecution
     let path : System.FilePath := { toString := path }
     let path ← IO.FS.realPath path
     let projectSearchPath ← getProjectSearchPath path
     searchPathRef.set projectSearchPath
-    let out := (← parseFile path).map ToJson.toJson
+    let out := (← parseFile path)
+    let firstSorry := out[0]?
+
+    -- do term elab
+    let expr: Expr := elabStringAsExpr rawExpr
 
     IO.println s!"Got sorries: {out}"
-
-    IO.eprintln s!"File extraction yielded"
-    IO.eprintln (toJson out)
+    kernelCheck path expr
   else
     IO.println "A path is needed."
 
